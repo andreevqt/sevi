9fe1ab6fab8edd84c417eeb0fe7e1ca63f98a046
Alex <qprquo@gmail.com>
2020-01-12 22:45:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Startuply - startup landing page
+This is an implementation of psd template provided by [Abdullah Un Noman ](https://dribbble.com/unnoman)
+
+## Build
+    npm install
+    npm run build
+## Local dev
+    npm run dev
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+import 'slick-carousel';
+import 'popper.js';
+import 'bootstrap/js/dist/tab';
+import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/dropdown';
+
+$(() => {
+  const body = $('body');
+});
