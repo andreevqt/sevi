130e2cc4e201efd16ae9a24e8da8d4c98fd44956
Alex <qprquo@gmail.com>
2020-01-18 21:17:01 +0300

    Add mobile menu

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import './bootstrap';
-import 'slick-carousel';
-import 'popper.js';
-import 'bootstrap/js/dist/tab';
-import 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/dropdown';
 
 $(() => {
   const body = $('body');
+  const menuToggle = $('.header__menu-toggle');
+  menuToggle.click((e) => {
+    body.addClass('mobile-menu-is-shown');
+  })
+
+  // dropdown
 });
