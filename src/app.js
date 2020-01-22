143fe0c79c8a8f44556db37fffde334ce83361d5
qprquo <qprquo@gmail.com>
2020-01-22 15:48:37 +0300

    Add menu transition

--- src/app.js
+++ src/app.js
@@ -11,25 +11,53 @@ AOS.init({
 
 $(() => {
   const body = $('body');
+
   const menuToggle = $('.header__menu-toggle');
-  menuToggle.click((e) => { 
-    body.addClass('mobile-menu-is-shown');
+  menuToggle.click((e) => {
+    body.toggleClass('mobile-menu-is-shown');
   })
-  const closeMenu = $('.header__close');
-  closeMenu.click((e) => {
-    body.removeClass('mobile-menu-is-shown');
-  });
+
   const handleToTop = () => {
     const offset = 100;
-    if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) {
+    if ($(window).scrollTop() > offset) {
       body.addClass('to-top');
     } else {
       body.removeClass('to-top');
     }
   }
+
   handleToTop();
+
   $(window).scroll(handleToTop);
   $('.button--to-top').click((e) => {
     $('html,body').animate({ scrollTop: 0 }, 'slow');
-  })
+  });
+
+  const header = $('.header');
+  const handleHeader = (e) => {
+
+    const scroll = $(window).scrollTop();
+
+    if (scroll >= 300) {
+      header.addClass('header--is-hidden');
+    } else {
+      header.removeClass('header--is-hidden');
+    }
+
+    if(scroll >= 400) {
+      header.addClass('header--transition');
+    } else {
+      header.removeClass('header--transition');
+    }
+
+    if (scroll >= 600) {
+      header.addClass('header--transition-start');
+    } else {
+      header.removeClass('header--transition-start');
+    }
+
+  };
+  handleHeader()
+
+  $(window).scroll(handleHeader);
 });
